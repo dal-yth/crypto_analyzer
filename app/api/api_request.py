8a4a9a919a1478818842bb9c8402206c362cab84
jmakela42 <dal@hive.fi>
2021-12-21 11:19:57 +0200

    granularity conversion

--- app/api/api_request.py
+++ app/api/api_request.py
@@ -1,5 +1,5 @@
 import requests
-from app.helpers.tools import create_params, create_url
+from app.helpers.tools import create_params, create_url, find_days_time, from_unixtime, handle_conversion
 
 
 class APIRequest:
@@ -12,6 +12,7 @@ class APIRequest:
 		return False
 
 	# request the data
+	# test the error cases!
 	def ranged_coin_data(self, args):
 		res = requests.get(url=create_url(args), params=create_params(args))
 		self.status_code = res.status_code
@@ -22,35 +23,67 @@ class APIRequest:
 		self.ranged_coin_data(args)
 		if self.request_failed():
 			return
-		volumes = [x[1] for x in self.json.get('total_volumes')]
+		data = handle_conversion(self.json.get('total_volumes'))
+		print("converted data:")
+		for item in data:
+			print(f"{from_unixtime(item[0])}:{item[1]}")
+		cumu = 0
+		for item in data:
+			cumu += item[1]
+		print(f"average volume: {cumu / len(data)}, cumu: {cumu}, len:{len(data)}")
+		print("")
+		days_time = find_days_time(data, args)
+		print("day by day data:")
+		for item in days_time:
+			print(f"{from_unixtime(item[0])}:{item[1]}")
+		volumes = [x[1] for x in days_time]
 		self.response = {'highest_volume': max(volumes, default=0)}
 
-	# not a fan of this but it works
+	# not a fan of this but it works, need to refactor and split
 	# counts days where trend goes downward and keeps track of highest value
 	def downward_trend(self, args):
 		self.ranged_coin_data(args)
 		if self.request_failed():
 			return
+		data = handle_conversion(self.json.get('prices'))
+		print("converted data:")
+		for item in data:
+			print(f"{from_unixtime(item[0])}:{item[1]}")
+		print("")
+		prices = find_days_time(data, args)
+		print("day by day data:")
+		for item in prices:
+			print(f"{from_unixtime(item[0])}:{item[1]}")
 		max_bearish = 0
 		counter = 0
-		prices = [x[1] for x in self.json.get('prices')]
 		for idx, val in enumerate(prices):
 			if idx == 0:
 				continue
-			if val > prices[idx-1]:
+			if val[1] < prices[idx-1][1]:
 				counter += 1
 			else:
-				if counter > max_bearish:
+				if counter > max_bearish: # counted more bearish days
 					max_bearish = counter
 				counter = 0
+		if counter > max_bearish: # in case last day was bearish
+			max_bearish = counter
 		self.response = {'max_bearish': max_bearish}
 
 	# find max difference in bitcoin value
+	# really need to refactor this
 	def max_profits(self, args):
 		self.ranged_coin_data(args)
 		if self.request_failed():
 			return
-		prices = list(self.json.get('prices'))
+		data = handle_conversion(self.json.get('prices'))
+		print("converted data:")
+		for item in data:
+			print(f"{from_unixtime(item[0])}:{item[1]}")
+		print("")
+		prices = find_days_time(data, args)
+		print("day by day data:")
+		for item in prices:
+			print(f"{from_unixtime(item[0])}:{item[1]}")
 		min = prices[0] if prices else 0 # get price of first entry
 		diff = 0
 		buy_date = None
@@ -62,6 +95,6 @@ class APIRequest:
 				diff = price[1] - min[1]
 				buy_date = min[0]
 				sell_date = price[0]
-		self.response = {"buy_date": buy_date, "sell_date": sell_date}
+		self.response = {"buy_date": from_unixtime(buy_date), "sell_date": from_unixtime(sell_date)}
 
 req = APIRequest()
--- app/helpers/tools.py
+++ app/helpers/tools.py
@@ -1,26 +1,36 @@
 import time
 from datetime import datetime
 
+day_and_hour = 86000 + 3600 # value for including the end date plus an hour
+
 # conversion to unixtime
 # if user gives wrong values or format, we simply return the original and let coingecko handle the error
-def to_unixtime(timestring):
+def to_unixtime(dt_string):
+    fmts = ["%Y-%m-%d", "%Y-%m-%d %H:%M:%S.%f"]
+    for fmt in fmts:
+        try:
+            d = datetime.strptime(dt_string, fmt)
+            return int(time.mktime(d.timetuple()))
+        except:
+            pass
+    return dt_string
+
+# conversion from unixtime and split to include only the day
+def from_unixtime(unixtime):
     try:
-        d = datetime.strptime(timestring, "%Y-%m-%d")
+        return datetime.fromtimestamp(unixtime).strftime('%Y-%m-%d %H:%M:%S.%f').split(" ")[0]
     except:
-        return timestring
-    unixtime = int(time.mktime(d.timetuple()))
-    return unixtime
+        return unixtime
 
-def from_unixtime(timestring):
-    return timestring
-
-# create params according to request args
+# create params according to request args and add the day and hour
 def create_params(args):
     params = {
         "vs_currency": args.get('vs_currency') or "eur",
         "from": to_unixtime(args.get('from')),
-        "to": to_unixtime(args.get('to')) # need to add 3600 to this
+        "to": to_unixtime(args.get('to'))
     }
+    if type(params['to']) == int: # if user gives invalid input can't add to string
+        params['to'] = params['to'] + day_and_hour # add day and hour to always get data for end date + hour into the next
     return params
 
 # create url for chosen coin
@@ -28,3 +38,30 @@ def create_url(args):
     coin_id = args.get('id') or "bitcoin"
     url = f"https://api.coingecko.com/api/v3/coins/{coin_id}/market_chart/range"
     return url
+
+# coingecko takes seconds but returns milliseconds
+def handle_conversion(data):
+    return [[int(item[0] / 1000), item[1]] for item in data] # this should work right?
+
+# check the level of granularity and need to find days time
+def check_range(args):
+    unix_day = 86000
+    start_unix = to_unixtime(args.get('from'))
+    end_unix = to_unixtime(args.get('to')) + unix_day
+    days = (end_unix - start_unix) / unix_day
+    return days
+
+# eww it's ugly but it works, refactor later
+def find_days_time(data, args):
+    if check_range(args) > 90: # amounts over 90 days work as is without finding days value
+        return data
+    low_hours = 7166 # 2 hours
+    high_hours = 10750 # 3 hours
+    unix_day = 86000
+    start_day = to_unixtime(args.get('from'))
+    days_values = []
+    for item in data:
+        if item[0] > start_day + low_hours and item[0] < start_day + high_hours:
+            days_values.append(item)
+            start_day = start_day + unix_day
+    return days_values
